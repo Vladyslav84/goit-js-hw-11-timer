1f39ef877f900c6b77fe05e5ba207c87b27de238
Vladyslav84 <72793544+Vladyslav84@users.noreply.github.com>
2021-05-10 18:20:34 +0300

    Зробив через клас

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-// });
 
 const clockFace = document.getElementById('clockFace');
 const clockDays = document.getElementById('clockDays');
@@ -9,52 +5,56 @@ const clockHours = document.getElementById('clockHours');
 const clockMins = document.getElementById('clockMins');
 const clockSesc = document.getElementById('clockSesc');
 
-// function init() {
-
-//     const stepCountDonw = countDown(time);
-
-//     updateClockFace(stepCountDonw);
-// };
 
 let time = 172801000;
 
-// class Timer {
-//     constructor() {
+class CountdownTimer {
+    constructor({ onTick }) {
+
+        this.onTick = onTick;
+    }
+
+    start() {
+        setInterval(() => {
+
+            time -= 1000;
+
+            const stepCountDonw = this.countDown(time);
+
+            this.onTick(stepCountDonw);
+
+        }, 1000);
+    };
 
 
-//     }
-// }
+    pad(value) {
+        return String(value).padStart(2, '0');
 
-setInterval(() => {
+    }
 
-    time -= 1000;
-    const stepCountDonw = countDown(time);
+    countDown(time) {
 
-    updateClockFace(stepCountDonw);
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-}, 1000);
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
 
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-
+        return { days, hours, mins, secs };
+    };
 }
 
+const countdownTimer = new CountdownTimer(
+    {
+        onTick: updateClockFace,
+    }
+);
 
-function countDown(time) {
 
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+countdownTimer.start();
 
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-    return scoreboard = { days, hours, mins, secs };
-};
 
 function updateClockFace({ days, hours, mins, secs }) {
 
